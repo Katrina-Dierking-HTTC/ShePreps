9b38900faab7e0e2d743219d6b3088d7e961f5aa
Katrina Dierking <kalynndier@gmail.com>
2021-02-07 21:32:36 -0500

    setting up pages

---
+++ home/src/components/main/header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header () {
+    return (
+        <div className = "header">
+            <h1> She Preps - Survival of the fitest-<span>ish</span></h1>
+        </div>
+    )
+}
